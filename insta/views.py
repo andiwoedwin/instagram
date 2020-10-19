e228664b23b59ffe66355f86b47f10362c68d6b3
andiwoedwin <andiwoedwin@gmail.com>
2020-10-20 02:01:37 +0300

    add user folder

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
 from .models import Post
 
 
@@ -7,6 +8,16 @@ def welcome(request):
         'posts': Post.objects.all()
     }
     return render(request, 'insta/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'insta/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request, 'insta/about.html',{'title':'About'})
