9f223af6762fa8f6945d3eea1644c7faa790e154
andiwoedwin <andiwoedwin@gmail.com>
2020-10-20 02:00:30 +0300

    add url to navigate to post details

--- insta/urls.py
+++ insta/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
-    path('', views.welcome, name='instagram-home'),
+    path('', PostListView.as_view(), name='instagram-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='instagram-about'),
 ]
